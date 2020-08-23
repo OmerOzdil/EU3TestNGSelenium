8e3c27c2a274b136286f2024645c5196483c1ad6
Omer Ozdil <omerozdil54@gmail.com>
2020-08-23 17:19:44 +0100

    Added

--- src/test/java/com/cybertek/tests/day4_basic_locators/nameLocatorTest.java
+++ src/test/java/com/cybertek/tests/day4_basic_locators/nameLocatorTest.java
@@ -5,12 +5,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class namaeLocatorTest {
+public class nameLocatorTest {
     public static void main(String[] args) {
         WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
         driver.get("http://practice.cybertekschool.com/sign_up");
 
         WebElement fullnameBox = driver.findElement(By.name("full_name"));
         fullnameBox.sendKeys("Mike Smith");
+
+        WebElement emailBox = driver.findElement(By.name("email"));
+        emailBox.sendKeys("omerozdil54@gmail.com");
+
+        WebElement signUpBUtton = driver.findElement(By.name("wooden_spoon"));
+        signUpBUtton.click();
+
+        driver.quit();
     }
 }
---
+++ src/test/java/com/cybertek/tests/day4_basic_locators/tagNameLocator.java
@@ -0,0 +1,23 @@
+package com.cybertek.tests.day4_basic_locators;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.sql.Driver;
+
+public class tagNameLocator {
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/sign_up");
+
+        WebElement fullNameInputBox = driver.findElement(By.tagName("input"));
+
+        fullNameInputBox.sendKeys("Mike Smith");
+
+        WebElement emailBox = driver.findElement(By.tagName("input[1]"));
+
+        emailBox.sendKeys("MikeSmith@gmail.com");
+    }
+}
