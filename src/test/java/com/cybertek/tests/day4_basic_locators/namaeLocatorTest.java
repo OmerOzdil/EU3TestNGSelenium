a339606a611d938782c93bf13a1a8ba2919afabe
Omer Ozdil <omerozdil54@gmail.com>
2020-08-23 16:18:58 +0100

    Added

--- src/test/java/com/cybertek/tests/day4_basic_locators/namaeLocatorTest.java
+++ src/test/java/com/cybertek/tests/day4_basic_locators/namaeLocatorTest.java
@@ -1,4 +1,16 @@
 package com.cybertek.tests.day4_basic_locators;
 
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class namaeLocatorTest {
+    public static void main(String[] args) {
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/sign_up");
+
+        WebElement fullnameBox = driver.findElement(By.name("full_name"));
+        fullnameBox.sendKeys("Mike Smith");
+    }
 }
