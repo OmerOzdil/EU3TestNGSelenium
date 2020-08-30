8e7afe0d75de9a41478c9e0d7b9b14a250f54a6d
Omer Ozdil <omerozdil54@gmail.com>
2020-08-30 20:05:21 +0100

    Added

--- src/test/java/com/cybertek/tests/day7_testng/BeforeAfterMethod.java
+++ src/test/java/com/cybertek/tests/day7_testng/BeforeAfterMethod.java
@@ -1,14 +1,26 @@
 package com.cybertek.tests.day7_testng;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class BeforeAfterMethod {
+
+    @BeforeClass
+    public void setUpClass() {
+        System.out.println("BEFORE CLASS");
+        System.out.println("executed one time before class");
+    }
+    @AfterClass
+    public void tearDownClas(){
+        System.out.println("AFTER CLASS");
+        System.out.println("executed one time after class");
+
+    }
+
     @Test
     public void test1(){
         System.out.println("First Test Case");
     }
+    //@Ignore
     @Test
     public void test2(){
         System.out.println("Second Test Case");
