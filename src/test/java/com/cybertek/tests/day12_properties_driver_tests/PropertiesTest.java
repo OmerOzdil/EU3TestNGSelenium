a1c72010732eb83552a8fd77e42ae0379a709f6b
Omer Ozdil <omerozdil54@gmail.com>
2020-09-20 16:29:22 +0100

    Added

---
+++ src/test/java/com/cybertek/tests/day12_properties_driver_tests/PropertiesTest.java
@@ -0,0 +1,12 @@
+package com.cybertek.tests.day12_properties_driver_tests;
+
+import org.testng.annotations.Test;
+
+public class PropertiesTest {
+
+    @Test
+    public void test1(){
+
+    }
+
+}
