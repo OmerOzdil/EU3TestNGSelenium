7cbc9385526123021af70d3a42971084366372cd
Omer Ozdil <omerozdil54@gmail.com>
2020-09-27 15:43:32 +0100

    Added

--- src/test/java/com/cybertek/pages/LoginPage.java
+++ src/test/java/com/cybertek/pages/LoginPage.java
@@ -1,5 +1,6 @@
 package com.cybertek.pages;
 
+import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -20,5 +21,22 @@ public class LoginPage {
     @FindBy(id = "_submit")
     public WebElement loginButton;
 
+
+    public void login(String username, String password){
+        usernameInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+        loginButton.click();
+    }
+
+    public void loginAsStoreManager(){
+        usernameInput.sendKeys(ConfigurationReader.get("storemanager_username"));
+        passwordInput.sendKeys(ConfigurationReader.get("storemanager_password"));
+        loginButton.click();
+    }
+    public void loginAsDriver(){
+        usernameInput.sendKeys(ConfigurationReader.get("driver_username"));
+        passwordInput.sendKeys(ConfigurationReader.get("driver_password"));
+        loginButton.click();
+    }
 
 }
